6afa2a9fefc4bdab273f350d562f3458f384592a
Donation App Deployer <deployer@example.com>
2025-07-12 02:07:52 +0300

    Updates

--- _app/immutable/chunks/4O1CicHx.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_19qyvvg)==null?void 0:s.base)??"/donation-app";var t;const a=((t=globalThis.__sveltekit_19qyvvg)==null?void 0:t.assets)??e;export{a as assets,e as base};
---
+++ _app/immutable/entry/start.DGMYI5Cj.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/6y0mv7b3.js";export{o as load_css,r as start};
