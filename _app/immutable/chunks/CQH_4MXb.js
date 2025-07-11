6446d703b661e48b29f1dbb50ee8bb76df6ab853
Donation App Deployer <deployer@example.com>
2025-07-12 02:17:09 +0300

    Updates

---
+++ _app/immutable/chunks/CQH_4MXb.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_f2lehr)==null?void 0:s.base)??"/donation-app";var e;const a=((e=globalThis.__sveltekit_f2lehr)==null?void 0:e.assets)??t;export{a as assets,t as base};
