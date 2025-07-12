7894542989bf6d0fb89267e88cf45e505df369a2
Donation App Deployer <deployer@example.com>
2025-07-12 04:37:27 +0300

    Updates

---
+++ _app/immutable/chunks/ChOet7mc.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1ks9we4)==null?void 0:s.base)??"/donation-app";var e;const a=((e=globalThis.__sveltekit_1ks9we4)==null?void 0:e.assets)??t;export{a as assets,t as base};
